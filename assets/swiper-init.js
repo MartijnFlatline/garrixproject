1292a3f1c86714847e6537c04718dbdeef5c3a37
MartijnFlatline <martijn@flatlineagency.com>
2024-09-05 04:56:36 -0700

    swiper update

--- assets/swiper-init.js
+++ assets/swiper-init.js
@@ -1,18 +1,8 @@
-import Swiper from 'swiper/bundle';
-import 'swiper/swiper-bundle.min.css';
-
 document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    speed: 5000,
-    autoplay: {
-      delay: 0,
-      disableOnInteraction: false,
-    },
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    loopAdditionalSlides: 5,
     direction: 'horizontal',
-    reverseDirection: true,
+    slidesperview: 4,
+    loop: true,
+    // freeMode: true,
   });
 });
