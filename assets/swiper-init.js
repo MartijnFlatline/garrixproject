844cda3d53dc0c7fbe0c30ade622f16325bbe9bc
MartijnFlatline <martijn@flatlineagency.com>
2024-09-05 06:58:57 -0700

    update

--- assets/swiper-init.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    slidesperview: 4,
-    loop: true,
-    // freeMode: true,
-  });
-});
